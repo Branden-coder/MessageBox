e6e376ff8e11f4309cc5f4faf5c5813076a2e41f
Branden van Staden <coolfornow@gmail.com>
2021-03-31 11:56:55 +0200

    Add project files.

---
+++ MessageBox/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace MessageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = "This a pop-up similar to JOptionPane";
+            string title = "JOptionPane Example";
+            System.Windows.Forms.MessageBox.Show(msg, title);
+
+
+        }
+    }
+}
